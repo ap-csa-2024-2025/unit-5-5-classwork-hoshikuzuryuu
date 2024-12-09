65d8bccadc4b5a8dc667b41a56c48a4ade16545a
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-12-09 10:18:22 -0800

    hello chat

--- Dog.java
+++ Dog.java
@@ -1,4 +1,40 @@
 public class Dog
 {
+   
+    private String breed;
+    private double size;
+    private String color;
 
+    public void setBreed(String inputBreed)
+    {
+        breed = inputBreed;   
+    }
+    public String breed()
+    {
+        return breed;
+    }
+    public void setSize(double inputSize) 
+    {
+        
+        size = inputSize;
+        System.out.println("Your dog's size is now set to " +  size);
+    }
+    public void setColor(String inputColor)
+    {
+
+        color = inputColor;
+        System.out.println("Your dog's color is now set to " +  color);
+    }
+    public void Bark() 
+    {
+        System.out.println("Woof!");
+    }
+    public void ChaseMailman()
+    {
+        System.out.println("Your canine companion gallops toward the mailman, whose peace is disturbed by the foe's sudden appearance.");
+    }
+    public void Wag()
+    {
+        System.out.println("In a fit of great joy, your canine companion's tail fwips back and forth swiftly, like an overclocked pendulum.");
+    }
 }
--- Main.java
+++ Main.java
@@ -4,6 +4,29 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create objects and test your classes in here
+    Scanner sc = new Scanner(System.in);
+    Point2D p1 = new Point2D();
+    Point2D p2 = new Point2D();
+    
+    p1.setX(0);
+    p1.setY(4);
+
+    p2.setX(3);
+    p2.setY(2);
+
+    Dog d = new Dog();
+    System.out.println("What's your dog's breed?");
+    d.setBreed(sc.nextLine());
+    System.out.println("Your dog's breed is now set to " + d.breed());
+    System.out.println("How many pounds is it?");
+    d.setSize(sc.nextDouble());
+    System.out.println("What color?");
+    d.setColor(sc.nextLine());
+
+    d.Wag();
+    d.Bark();
+    d.ChaseMailman();
+
+    sc.close();
   }
 }
